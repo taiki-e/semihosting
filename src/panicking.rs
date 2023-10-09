025d6dd15cbe918b52450ca2204c1a4d8e82458a
Taiki Endo <te316e89@gmail.com>
2023-10-09 20:56:34 +0900

    Tweak panic_handler

--- src/panicking.rs
+++ src/panicking.rs
@@ -5,12 +5,13 @@
 fn _panic(_info: &core::panic::PanicInfo<'_>) -> ! {
     #[cfg(feature = "stdio")]
     eprintln!("{_info}");
+
     #[cfg(feature = "panic-unwind")]
     {
         use crate::atomic::Ordering;
-        // PANICKED is global, so if panics occur concurrently on thread 1 and thread 2 this could
-        // be mistakenly thought to be a double panic. However, I'm not sure if there is a way to
-        // handle that well without thread local.
+        // TODO: PANICKED is global, so if panics occur concurrently on thread 1 and thread 2 this
+        // could be mistakenly thought to be a double panic. However, I'm not sure if there is a way
+        // to handle that well without thread local.
         if crate::experimental::panic::PANICKED.fetch_add(1, Ordering::AcqRel) != 0 {
             #[cfg(feature = "stdio")]
             eprintln!("panic during panic, aborting");
@@ -19,7 +20,27 @@ fn _panic(_info: &core::panic::PanicInfo<'_>) -> ! {
     }
 
     #[cfg(feature = "backtrace")]
-    stack_trace();
+    {
+        use core::{ffi::c_void, ptr};
+
+        use unwinding::abi::{UnwindContext, UnwindReasonCode, _Unwind_Backtrace, _Unwind_GetIP};
+
+        extern "C" fn callback(
+            unwind_ctx: &UnwindContext<'_>,
+            _arg: *mut c_void,
+        ) -> UnwindReasonCode {
+            let ip = _Unwind_GetIP(unwind_ctx);
+            if ip == 0 {
+                UnwindReasonCode::NORMAL_STOP
+            } else {
+                eprintln!("  {ip:#x}");
+                UnwindReasonCode::NO_REASON
+            }
+        }
+
+        eprintln!("stack backtrace:");
+        _Unwind_Backtrace(callback, ptr::null_mut());
+    }
 
     #[cfg(feature = "panic-unwind")]
     {
@@ -27,25 +48,6 @@ fn _panic(_info: &core::panic::PanicInfo<'_>) -> ! {
         #[cfg(feature = "stdio")]
         eprintln!("failed to begin panic (unwind error code {})", _code.0);
     }
+
     crate::process::exit(101)
-}
-
-#[cfg(feature = "backtrace")]
-fn stack_trace() {
-    use core::{ffi::c_void, ptr};
-
-    use unwinding::abi::{UnwindContext, UnwindReasonCode, _Unwind_Backtrace, _Unwind_GetIP};
-
-    extern "C" fn callback(unwind_ctx: &UnwindContext<'_>, _arg: *mut c_void) -> UnwindReasonCode {
-        let ip = _Unwind_GetIP(unwind_ctx);
-        if ip == 0 {
-            UnwindReasonCode::NORMAL_STOP
-        } else {
-            eprintln!("  {ip:#x}");
-            UnwindReasonCode::NO_REASON
-        }
-    }
-
-    eprintln!("stack backtrace:");
-    _Unwind_Backtrace(callback, ptr::null_mut());
 }
